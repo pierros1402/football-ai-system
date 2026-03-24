945958ee1e0341779ec8df47fb8af5850867bc1a
pierros1402 <pierros1402@gmail.com>
2026-03-24 15:51:02 +0200

    Initial clean upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from .config import settings
 from .routers import auth
+from .database import Base, engine
+from . import models
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(title=settings.app_name)
 
